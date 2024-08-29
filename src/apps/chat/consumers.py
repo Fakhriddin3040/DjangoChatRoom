636f07519fb93cb66f7a04646139f457577c8178
Fakhriddin <fahriddin3040@gmail.com>
2024-08-29 11:24:49 +0500

    Implemented online status changing and some animatinons.
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- src/apps/chat/consumers.py
+++ src/apps/chat/consumers.py
@@ -11,6 +11,7 @@ from src.utils.functions.models import a_try_to_get_object
 
 class ChatConsumer(AsyncWebsocketConsumer):
     channel_layer: RedisChannelLayer
+    chat: Chat
 
     async def connect(self):
         self.user = self.scope["user"]
@@ -21,7 +22,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.chat = await a_try_to_get_object(
             manager=Chat.objects, exception=Chat.DoesNotExist, title=self.chat_name
         )
+        await self.update_online_users(connected=True)
+        await self.update_online_count()
         await self.accept()
+
+    async def update_online_users(self, connected: bool) -> None:
+        if connected:
+            if not await self.user_exists_in_chat(self.user.id):
+                await self.chat.users_online.aadd(self.user)
+        else:
+            if await self.user_exists_in_chat(self.user.id):
+                await self.chat.users_online.aremove(self.user)
+
+    async def user_exists_in_chat(self, user_id: int) -> bool:
+        return await self.chat.users_online.filter(id=user_id).aexists()
+
+    async def update_online_count(self) -> None:
+        event: Dict[str, Any] = {
+            "type": "online_count_handler",
+            "online_count": await self.chat.users_online.acount() - 1
+        }
+        await self.channel_layer.group_send(
+            self.chat_name, event
+        )
+
+    async def online_count_handler(self, event: Dict[str, Any]) -> None:
+        context = self.get_template_context(online_count=event["online_count"])
+        html = await self.render_to_string("chat/partials/online_count.html", context)
+
+        await self.send(text_data=html)
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
@@ -38,20 +67,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.chat_name, event
         )
 
+    def get_template_context(self, **kwargs) -> Dict[str, Any]:
+        return {
+            "user": self.user,
+            **kwargs,
+        }
+
     async def message_handler(self, event: Dict[str, Any]) -> None:
         message = await self.get_message(id=event["message_id"])
-        context = {
-            "message": message,
-            "user": self.user,
-        }
-        html = await sync_to_async(render_to_string)("chat/partials/message.html", context=context)
+        context = self.get_template_context(message=message)
+        html = await self.render_to_string("chat/partials/message.html", context)
+
         await self.send(text_data=html)
-        
+
+    async def render_to_string(self, template_name: str, context: Dict[str, Any], **kwargs: Dict[str, Any]):
+        return await sync_to_async(render_to_string)(template_name=template_name, context=context, **kwargs)
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.chat_name, self.channel_name
         )
+        await self.update_online_users(connected=False)
+        await self.update_online_count()
 
     async def get_message(self, id: int) -> Message:
         return await Message.objects.aget(id=id)
--- src/apps/chat/models/chat.py
+++ src/apps/chat/models/chat.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Chat(models.Model):
     title: str = models.CharField(max_length=128, verbose_name="Название чата")
+    users_online = models.ManyToManyField(
+        "authorization.User",
+        related_name="chats_online",
+        verbose_name="Пользователи онлайн",
+        )
 
     def __str__(self):
         return self.title
